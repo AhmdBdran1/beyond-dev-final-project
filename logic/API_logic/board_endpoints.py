49d0f6a4def9ab2a64ed1bdffd8cbe7b4ef13ddd
AhmdBdran1 <ahmdbdran@h-Air-sl-user.Dlink>
2024-03-22 16:57:50 +0200

    try to fixx

--- logic/API_logic/board_endpoints.py
+++ logic/API_logic/board_endpoints.py
@@ -6,14 +6,10 @@ from utils import read_from_secret_file
 class BoardEndPoints:
     config = read_from_secret_file()
     token = config['website_token']
-    api_key= config['api_token']
+    api_key = config['api_token']
     # Constants for Trello API
     BASE_URL = 'https://api.trello.com/1'
     CREATE_BOARD_URL = f'{BASE_URL}/boards/'
-    #api_key = "ABBAbbe530cb763d9199352c6463e3440ac8"
-    #token = "ABBAATTA4f14fcd1933bde89ab95ad5f3003ae9e2c09081ad8079f39b3865259dc47671420A43297"
-    #token = token[4:]  # Extract from the 4th character (index 3) to the end
-    #api_key = api_key[4:]
 
     def create_new_board(self):
         print(self.token)
@@ -30,5 +26,3 @@ class BoardEndPoints:
         response = requests.post(self.CREATE_BOARD_URL, params=params)
 
         return response
-
-
